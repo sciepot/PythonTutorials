6f0a1d99733ee3c97598fcf339e648d82c78a2bd
Vyacheslav Kim <kimslava@unist.ac.kr>
2023-09-06 20:50:49 +0600

    cry

--- 3_Loops/Codes_2/sortingWords.py
+++ 3_Loops/Codes_2/sortingWords.py
@@ -9,16 +9,20 @@
 
     выходные данные
     [apple, banana, orange, pineapple]
+
+[4, 1, 3, 5]
+
 '''
 
 def sortAl(l):
-    # Здесь начинается ваш код
-    for i in range(len(l)):
-        for x in range(i + 1, len(l)):
+    [1, 2, 3, 3, 5] # 5 > 3, 3 < 5
+    # Здесь начинается ваш код bubble sort 
+    for i in range(len(l)): # 0 1 2 3 4
+        for x in range(i + 1, len(l)): # 1 2 3 4
             if l[i] > l[x]:
-                temp = l[x]
-                l[x] = l[i]
-                l[i] = temp
+                temp = l[i]
+                l[i] = l[x]
+                l[x] = temp
     # Здесь заканчиваеся ваш код
     return l
 
---
+++ 4_Functions/Codes/Fibonacci.py
@@ -0,0 +1,6 @@
+def fib(n): # 1 1 2 3 5 8     n-1 n-2 n
+    
+
+    return fib(n - 2) + fib(n-1)
+
+fib_n = fib_(n - 1) + fib_(n - 2)
